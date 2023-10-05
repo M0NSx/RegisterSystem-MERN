3c6d998c277a152a7f73a6b9ccb7e361d49de27e
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-05 18:04:10 -0300

    Update login.jsx

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -6,3 +6,5 @@ import ImageBG from './img/BF-BG2.png';
 import { Link } from 'react-router-dom';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios';
+
+export default function Login() {
