4bb20155da4fb9f361f0a2434a1f5ce6c37c3d5c
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-04 21:43:46 -0300

    Update register.jsx

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -3,3 +3,4 @@ import './Register.css'
 import ImageL from './img/Site-logo.png';
 import ImageBG from './img/BF-BG2.png';
 import { Link } from 'react-router-dom';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
