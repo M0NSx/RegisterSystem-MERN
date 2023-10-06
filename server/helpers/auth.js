3d2fcd9408d86794aec887e1b7b719403b699ece
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-06 17:28:32 -0300

    Update auth.js

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -4,3 +4,5 @@ const hashPassword = (password) => {
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
             if(err) {
+                reject(err)
+            }
