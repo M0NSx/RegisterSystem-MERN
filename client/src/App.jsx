1f8ee2661f97d7b8ee2e8e3c5eef6d9d3b1ce482
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-07 18:37:31 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Register from './register'
 import Login from './login'
 import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
+import { UserContextProvider } from '../context/userContext'
 
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
@@ -13,12 +14,14 @@ function App() {
 
   return (
     <>
-    <Toaster position='bottom-right' toastOptions={{duration: 2000}}/>
-    <Routes>
-      <Route path='/' element={<Dashboard />}/>
-      <Route path='/register' element={<Register />}/>
-      <Route path='/login' element={<Login />}/>
-    </Routes>
+    <UserContextProvider>
+      <Toaster position='bottom-right' toastOptions={{duration: 2000}}/>
+      <Routes>
+        <Route path='/' element={<Dashboard />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </UserContextProvider>
     </>
   )
 }
