d3a2664cda03aa984341baeb357ee5baa9551d4a
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-07 18:34:37 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { mongoose } = require('mongoose');
+const cookieParser = require('cookie-parser')
+const app = express();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Database connected'))
 .catch((err) => console.log('Database not connected', err))
 
-const app = express();
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/authRoutes'))
 
